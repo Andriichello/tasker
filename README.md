04fa90b63398365874e1750394b0fb478d9d2877
Andrii Prykhodko <andriichello@gmail.com>
2025-07-21 09:36:46 +0300

    initial: setup Laravel project

---
+++ README.md
@@ -0,0 +1,3 @@
+## `tasker`
+
+A minimal task management system that allows users to create and track tasks.
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'First User',
+            'email' => 'first@example.com',
+            'password' => 'secret',
+        ]);
+
+        User::factory()->create([
+            'name' => 'Second User',
+            'email' => 'second@example.com',
+            'password' => 'secret',
+        ]);
+
+        User::factory()->create([
+            'name' => 'Third User',
+            'email' => 'third@example.com',
+            'password' => 'secret',
+        ]);
+    }
+}
